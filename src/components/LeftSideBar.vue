ad6aa12512ea4ff28e0fd4c941bf5c217a9ed6ff
geekmaros <razicruz@gmail.com>
2020-02-20 21:03:38 +0100

    modified pages

--- src/components/LeftSideBar.vue
+++ src/components/LeftSideBar.vue
@@ -5,9 +5,9 @@
     <section class="profile">
       <div class="user">
         <img
-          class="user__avatar"
-          src="../assets/avatars/Avatar.png"
-          alt="User_Avatar"
+                class="user__avatar"
+                src="../assets/avatars/Avatar.png"
+                alt="User_Avatar"
         />
         <span class="ml-8 user__name">Ivan Bryan</span>
         <i class="fa fa-angle-down ml-4"></i>
@@ -18,55 +18,61 @@
       </div>
     </section>
 
-    <section class="direct__message">
-      <div class="section">
-        <span class="section__header">Direct Messages</span>
-        <i class="fa fa-plus"></i>
-      </div>
+    <div class="subleftbar">
 
-      <div class="profiles">
-        <div v-for="i in 7" class="profile" :key="i">
-          <div class="user">
-            <img
-              class="user__avatar"
-              src="../assets/avatars/Avatar.png"
-              alt="User_Avatar"
-            />
-            <span class="ml-8 user__name">Ivan Bryan</span>
-          </div>
 
-          <div class="online__status" :class="{'offline': i%2 == 0}">
-            <i class="fa fa-circle"></i>
+      <section class="direct__message">
+        <div class="section">
+          <span class="section__header">Direct Messages</span>
+          <i class="fa fa-plus cursor-pointer"></i>
+        </div>
+
+        <div class="profiles">
+          <div v-for="i in 7" class="profile" :key="i">
+            <div class="user">
+              <img
+                      class="user__avatar"
+                      src="../assets/avatars/Avatar.png"
+                      alt="User_Avatar"
+              />
+              <span class="ml-8 user__name">Ivan Bryan</span>
+            </div>
+
+            <div class="online__status" :class="{'offline': i%2 == 0}">
+              <i class="fa fa-circle"></i>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-    <section class="channels">
-      <div class="section mb-4">
-        <span class="section__header">Channels</span>
-        <i class="fa fa-plus"></i>
-      </div>
+      <section class="channels">
+        <div class="section mb-4">
+          <span class="section__header">Channels</span>
+          <i class="fa fa-plus cursor-pointer"></i>
+        </div>
 
-      <ul class="hashtag">
-        <li v-for="(channel, i) in channels" class="list" :key="i">
-          <div>
-            <span class="hash mr-8">#</span><span>{{channel}}</span>
-          </div>
+        <ul class="hashtag">
+          <li @click="selectedChannel = i"  :class="{'list__selected': selectedChannel === i}" v-for="(channel, i) in channels" class="list cursor-pointer" :key="i">
+            <div>
+              <span class="hash mr-8">#</span><span>{{channel}}</span>
+            </div>
 
-          <div v-show='i%2 == 0' class="ellipse">
-            <span class="ellipse_text">1</span>
-          </div>
-        </li>
-      </ul>
-    </section>
+            <div v-show='i%2 == 0' class="ellipse">
+              <span class="ellipse_text">1</span>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </div>
   </section>
 </template>
 <script>
 export default {
   data() {
     return {
-      channels: ['Relationship', 'Sport', 'Entertainment', 'Tech', 'SavagePosts', 'Lifestyle', 'Travels']
+      channels: ['Relationship', 'Sport', 'Entertainment', 'Tech', 'SavagePosts', 'Lifestyle', 'Travels'],
+      selectedChannel: ''
+
     };
   }
 };
--- src/components/MainBar.vue
+++ src/components/MainBar.vue
@@ -1,6 +1,71 @@
 <template>
     <section class="main-bar">
+        <div class="top-bar mt-8">
+            <div class="top-left">
+                <span class="top-left-text"># Relationship</span> <span class="light-text ml-4">|</span> <i class="fa fa-user ml-4 "></i> <span class="light-text">55</span>
+                <p class="top-left-subtext">Lets talk about love and other things maybe.</p>
+            </div>
+            <div class="top-right">
+                <div class="search">
+                        <i class="fa fa-search"></i>
 
+                        <input class="search__bar" type="search" placeholder=" Search for friend or chat">
+                </div>
+            </div>
+        </div>
+        <p class="date__line"> Today 11:40AM</p>
+        <div class="chat__area mt-8">
+                <div v-for="i in 2" :key="i" class="left_chat_user flex align-items-center mb-24">
+
+                    <div class="profile">
+                        <img
+                                class="user__avatar"
+                                src="../assets/avatars/Avatar@2x.png"
+                                alt="User_Avatar"
+                        />
+
+                    </div>
+                    <div class="user_chat_details">
+                        <span class="ml-8 user__chat">My name is Anne , I think your playlist slaps alot! My name is Anne , I think your playlist slaps alot! </span>
+                        <span class="ml-4 chat__date">Tosin F Aug 8, 2019, 7:01 PM</span>
+                        <span class="ml-4 chat__reaction">has joined</span>
+                    </div>
+
+                </div>
+
+
+                <div v-for="i in 5" :key="i" class="right_chat_user  mb-24">
+
+                    <div class="user_chat_details">
+                        <span class="ml-8 user__chat">This is a very nice Chat Application</span>
+                        <span class="ml-4 chat__date">Tosin F Aug 8, 2019, 7:01 PM</span>
+
+                    </div>
+                    <img
+                            class="user__avatar ml-16"
+                            src="../assets/avatars/Avatar@2x.png"
+                            alt="User_Avatar"
+                    />
+
+                </div>
+
+
+
+
+
+        </div>
+
+        <div class="search mt-32">
+
+            <input class="search__bar" type="search" placeholder=" Search for friend or chat">
+
+            <div class="icon">
+                <i class="ml-24 fa fa-pencil"></i>
+                <i class="ml-24 fa fa-picture-o"></i>
+                <i class="ml-24 fa fa-user"></i>
+            </div>
+<!--            <i class=" ml-24 fa fa-user🙂️">🙂️</i>-->
+        </div>
     </section>
 </template>
 <script>
@@ -13,5 +78,5 @@
     }
 </script>
 <style lang="sass" scoped>
-    @import "../assets/components/RightSideBar"
+    @import "../assets/components/MainBar"
 </style>
